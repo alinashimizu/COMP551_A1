3472b249f8949256dda3ab950b7f5f7605dd342f
Alina Shimizu <alinashimizu@Alinas-MacBook-Air-3.local>
2025-01-25 16:33:41 -0500

    Done initial data analysis, file could be cleaned up a bit more

--- data_cleaning.py
+++ data_cleaning.py
@@ -21,10 +21,10 @@ y = heart_disease.data.targets
 
 #clean data - check for missing features
 
-#print("Missing feature values:")
+#print("missing feature values:")
 #print(X.isna().sum())
 
-#print("Missing target values")
+#print("missing target values")
 #print(y.isna().sum())
 
 #Option 1 - Drop missing values
@@ -41,9 +41,9 @@ y_clean2 = y
 
 
 #other cleaning processes to be completed: remove duplicates, outliers, invalid values
-print("Check for outliers")
+#print("Check for outliers")
 #for some reason i couldnt include ca, but i got the other int features, also do we need to remove outliers?
-X_clean.boxplot(column=['age', 'trestbps', 'chol', 'thalach', 'oldpeak'])
+#X_clean.boxplot(column=['age', 'trestbps', 'chol', 'thalach', 'oldpeak'])
 #plt.show()
 
 #categorical variables
@@ -77,25 +77,67 @@ msd_ranked = mean_sqd_diff.sort_values(ascending=False)
 
 #***DATASET 2 - Penguin Dataset for Multi-Class***
 data = pd.read_csv('/Users/alinashimizu/Downloads/penguins_size.csv')
+
+
+#Mapping here to change sex into a numerical variable 
 X_penguin = data.drop(columns=['species', 'island'])
+X_penguin['sex'] = X_penguin['sex'].map({'MALE': 1, 'FEMALE': 0})
 y_penguin = data['species']
 
+
 #Option 1 - Drop na rows 
-X_penguin_clean = X_penguin.dropna() 
-y_penguin_clean = y.loc[X_clean.index]
+X_penguin_clean = X_penguin.copy().dropna() 
+y_penguin_clean = y_penguin.copy().loc[X_penguin_clean.index]
 #print(y_penguin_clean.head())
 #print(X_penguin_clean.head())
 
 #Option 2 - Fill NA rows with average 
 X_penguin_clean2 = X_penguin.copy()
-numeric_columns = X_penguin.select_dtypes(include=['float64', 'int64']).columns
-X_penguin_clean2[numeric_columns] = X_penguin[numeric_columns].fillna(X_penguin[numeric_columns].mean())
+numeric_columns = ['culmen_length_mm', 'culmen_depth_mm', 'flipper_length_mm']
+X_penguin_clean2[numeric_columns] = X_penguin_clean2[numeric_columns].fillna(X_penguin_clean2[numeric_columns].mean())
 #handle sex column differently 
-X_penguin_clean2['sex'] = X_penguin['sex'].fillna(X_penguin['sex'].mode()[0])
+X_penguin_clean2['sex'] = X_penguin_clean2['sex'].fillna(X_penguin_clean2['sex'].mode().iloc[0])
 
-y_penguin_clean2 = y_penguin
+y_penguin_clean2 = y_penguin.copy()
 #y can stay the same because there are no missing target values
-print(y_penguin_clean2.head())
-print(X_penguin_clean2.head())
 
 #other cleaning processes to be completed: remove duplicates, outliers, invalid values
+#X_penguin_clean2.boxplot(column=numeric_columns)
+#plt.show()
+#this was hard to visualize since the numbers are on very different scales, but could go back and normalize 
+
+#checked for duplicates, all False and all values valid 
+
+#Compute means of each feature in each species group: Need to concat back into one df first 
+cleaned_penguin_data = pd.concat([X_penguin_clean2, y_penguin_clean2], axis=1)
+
+#helper function for multi-class stats 
+def penguin_stats(group):
+    stats = group.mean(numeric_only=True) 
+    stats['sex'] = group['sex'].mode()[0] 
+    return stats
+
+grouped_stats = cleaned_penguin_data.groupby('species').apply(penguin_stats)
+
+print(grouped_stats)
+
+#compute all the squared differences 
+squared_differences = {}
+
+#sum of squared differences, should also maybe do pairwise differences 
+
+for col in X_penguin_clean2.columns:
+    means = grouped_stats[col]
+    diff_sum = 0
+    for i in range(len(means)):
+        for j in range(i + 1, len(means)):
+            diff_sum += (means.iloc[i] - means.iloc[j]) ** 2 #squared difference between group means
+    squared_differences[col] = diff_sum
+
+rank_features = sorted(squared_differences.items(), key=lambda x: x[1], reverse=True)
+
+print("Ranked Features")
+for feature, score in rank_features:
+    print(f"{feature}: {score}")
+
+
